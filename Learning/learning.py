f037b7263da86df01e9f2ecd69857ad7f6b0ece3
Jawadhrz <jawadtechexpertz@gmail.com>
2024-05-10 13:19:59 +0500

    first commit

---
+++ Learning/learning.py
@@ -0,0 +1,80 @@
+# Why Python?
+# Easy to learn             # Web development
+# Scripting  and Automation # Data Science
+# Supports Graphic and Visualizations
+# Extensible and Portable
+# Abundat Libraries and Frameworks
+
+#-----------------
+
+
+# Python first version - 0.9.0 --> Feb 1991
+# Python first version - 3.12.3 --> Oct 2023
+ 
+
+#------------------
+
+#Basics  
+
+# this is a comment
+# first program
+print("Hello, World!")
+
+#-----------------
+
+#python Variables
+x = 10
+y = 5.5
+z = "Hello"
+print(x, y, z)
+
+#-----------------
+
+#python Data Types
+# int = 123
+# float = 12.3
+# complex = 12 + 4j
+# string  = "abc"
+# list = [1, "abcd", 2.2]
+# tuple = (1, "abc", 22.4)
+# dict = {"key":"value"}
+# set = {1,2,3}
+# bool = False
+
+#-------------------
+
+# Operators in python
+
+# Arithmetic Operators
+print(5 + 3)
+print(5 - 3)
+print(5 * 3)
+print(5 / 3)
+print(5 % 3)
+print(5 // 3)
+print(5 ** 3)
+
+# Assignment Operators
+ax = 5
+ax +=3
+ax %=3
+ax **=3
+ax //=3
+print(ax)
+
+# Comparison Operators
+print(5>3)
+print(5<3)
+print(5==3)
+print(5!=3)
+print(5<=3)
+print(5>=3)
+
+
+# Logical Operators
+print(True and False) # Logical AND, Output: False
+print(True or False) # Logical OR, Output: True
+print(not True) # Logical NOT, Output: False
+
+
+#------------------
